125f46d4310ee7ea1439ab6983cc8f578a7cda80
Lisa <zuzatoy@gmail.com>
2018-10-08 11:23:50 +1300

    local component store

--- src/App.js
+++ src/App.js
@@ -1,28 +1,70 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
+
+const list = [
+  {
+    title: 'React',
+    url: 'https://reactjs.org/',
+    author: 'Jordan Walke',
+    num_comments: 3,
+    points: 4,
+    objectID: 0,
+}, {
+    title: 'Redux',
+    url: 'https://redux.js.org/',
+    author: 'Dan Abramov, Andrew Clark',
+    num_comments: 2,
+    points: 5,
+    objectID: 1,
+}, ];
+
+const cars = [
+  {
+    title: 'Mazda',
+    color: 'red',
+    brand: 'new'
+  },
+  {
+    title: 'Toyota',
+    color: 'brown',
+    brand: 'old'
+  }
+]
+
+
+
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      list: list,
+      cars: cars
+
+    };
+  }
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+        {this.state.cars.map(car =>
+          <div key ={car.title} >
+            <span>{car.title}</span>
+            <span>{car.color}</span>
+          </div>
+        )}
+
+        {this.state.list.map(item =>
+          <div key={item.objectID}>
+            <span>
+              <a href={item.url}>{item.title}</a>
+            </span>
+            <span>{item.author}</span>
+            <span>{item.num_comments}</span>
+            <span>{item.points}</span>
+      </div> 
+            )       }
+    </div> );
+        }  
 }
 
 export default App;
