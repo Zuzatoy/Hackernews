c2e41303caeb0090919e879cd9498cf4cdadaddd
Lisa <zuzatoy@gmail.com>
2018-10-09 19:46:55 +1300

    add style

--- src/App.js
+++ src/App.js
@@ -67,44 +67,25 @@ class App extends Component {
   render() {
     const { searchTerm, list } = this.state;
     return (
-      <div className="App">
-      <Search
-        value={searchTerm}
-        onChange={this.onSearchChange}
-        />
-        <Table
+      <div className="page">
+        <div className="interactions">
+          <Search
+            value={searchTerm}
+            onChange={this.onSearchChange}
+          > Search
+          </Search>
+        </div>
+          <Table
           list={list}
           pattern={searchTerm}
           onDismiss={this.onDismiss}
           />
-      <form>
-        <input type='text'
-        value={searchTerm} 
-        onChange={this.onSearchChange}
-        />
-      </form>
-        {this.state.cars.map(car =>
-          <div key ={car.title} >
-            <span>{car.title}</span>
-            <span>{car.color}</span>
-            {/* <span>
-              <button onClick={() => this.onDismiss(car.title)}
-              >
-              Dismiss
-              </button>
-            </span> */}
-          </div>
-        )}
+        </div>
+        ); 
+      }
+    }
 
-
-    </div> );
-        }  
-}
-
-class Search extends Component {
-  render() {
-    const { value, onChange, children } = this.props;
-    return (
+const Search = ({ value, onChange, children }) =>
       <form>
         {children} <input
           type='text'
@@ -112,54 +93,59 @@ class Search extends Component {
           onChange={onChange}
         />
       </form>
-    )
-  }
-}
+    
+  
+
 
 export default App;
 
 
-class Table extends Component {
-  render() {
-    const { list, pattern, onDismiss } = this.props;
-    return (
-      <div>
+const Table = ({ list, pattern, onDismiss }) =>  
+      <div className='table'>
         {list.filter(isSearched(pattern)).map(item =>
-          <div key={item.objectID}>
-            <span>
+          <div key={item.objectID} className='table-row'>
+            <span style={largeColumn}>
               <a href={item.url}>{item.title}</a>
             </span>
-            <span>{item.author}</span>
-            <span>{item.num_comments}</span>
-            <span>{item.points}</span>
-            <span>
+            <span style={midColumn}>
+            {item.author}
+            </span>
+            <span style={smallColumn}>
+            {item.num_comments
+            }</span>
+            <span style={smallColumn}>
+            {item.points
+            }</span>
+            <span style={smallColumn}>
               <Button onClick={() => this.onDismiss(item.objectID)}>
               Dismiss
               </Button>
             </span>
 
       </div> 
-            )       }
+            )  }
       </div>
-    )
-  }
-}
+    
 
-class Button extends Component {
-  render() {
-    const {
-      onClick,
-      className = '',
-      children,
-    } = this.props;
-    return (
+    const largeColumn = { 
+      width: '40%',
+    };
+    const midColumn = { 
+      width: '30%',
+    };
+    const smallColumn = { 
+      width: '10%',
+    };
+
+
+
+const Button = ( {onClick, className, children }) =>  
       <button
         onClick = {onClick}
         className = {className}
         type="button"
         >
         {children}
-        </button>
-    )
-  }
-}
+      </button>
+    
+  
