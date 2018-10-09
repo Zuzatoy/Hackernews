b1a8008a62043c44f8e3b777f9691157bee2151a
Lisa <zuzatoy@gmail.com>
2018-10-09 13:59:56 +1300

    distructor

--- src/App.js
+++ src/App.js
@@ -65,6 +65,7 @@ class App extends Component {
   }
 
   render() {
+    const { searchTerm, list } = this.state;
     return (
       <div className="App">
       <form>
@@ -85,7 +86,7 @@ class App extends Component {
           </div>
         )}
 
-        {this.state.list.filter(isSearched(this.state.searchTerm)).map(item =>
+        {list.filter(isSearched(searchTerm)).map(item =>
           <div key={item.objectID}>
             <span>
               <a href={item.url}>{item.title}</a>
