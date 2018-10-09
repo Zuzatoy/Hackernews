0e0299e0f53b154ce01f37c24899d415d80b5fdc
Lisa <zuzatoy@gmail.com>
2018-10-09 21:25:26 +1300

    add mounting

--- src/App.js
+++ src/App.js
@@ -33,6 +33,12 @@ const cars = [
 ]
 
 
+
+const DEFAULT_QUERY = 'redux';
+const PATH_BASE = 'https://hn.algolia.com/api/v1';
+const PATH_SEARCH = '/search';
+const PARAM_SEARCH = 'query=';
+
 function isSearched(searchTerm) {
   return function (item) {
     return item.title.toLowerCase().includes(searchTerm.toLowerCase());
@@ -44,14 +50,28 @@ class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      list,
-      cars,
-      searchTerm: ''
+      result: null,
+      searchTerm: DEFAULT_QUERY,
 
     };
+    this.setSearchTopStories = this.setSearchTopStories.bind(this);
     this.onDismiss = this.onDismiss.bind(this);
     this.onSearchChange = this.onSearchChange.bind(this);
   }
+
+  setSearchTopStories(result) {
+    this.setState({ result });
+  }
+
+  componentDidMount() {
+    const { searchTerm } = this.state;
+
+    fetch(`${PATH_BASE}${PATH_SEARCH}?${PARAM_SEARCH}${searchTerm}`)
+    .then(response => response.json())
+    .then(result => this.setSearchTopStories(result))
+    .catch(error => error);
+  }
+
 
   onDismiss(id) {
     const isNotId = item => item.objectID !== id;
@@ -65,7 +85,9 @@ class App extends Component {
   }
 
   render() {
-    const { searchTerm, list } = this.state;
+    const { searchTerm, result } = this.state;
+
+    if (!result) { return null; }
     return (
       <div className="page">
         <div className="interactions">
