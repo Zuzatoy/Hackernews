88fcfb067ada572ce6d1d0348bacfd55f973d629
Lisa <zuzatoy@gmail.com>
2018-10-09 13:49:01 +1300

    forms

--- src/App.js
+++ src/App.js
@@ -33,16 +33,24 @@ const cars = [
 ]
 
 
+function isSearched(searchTerm) {
+  return function (item) {
+    return item.title.toLowerCase().includes(searchTerm.toLowerCase());
+  }
+}
+
 
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      list: list,
-      cars: cars
+      list,
+      cars,
+      searchTerm: ''
 
     };
     this.onDismiss = this.onDismiss.bind(this);
+    this.onSearchChange = this.onSearchChange.bind(this);
   }
 
   onDismiss(id) {
@@ -51,9 +59,19 @@ class App extends Component {
     this.setState({ list: updatedList });
   }
 
+  onSearchChange(event) {
+    this.setState({ searchTerm: event.target.value });
+
+  }
+
   render() {
     return (
       <div className="App">
+      <form>
+        <input type='text' 
+        onChange={this.onSearchChange}
+        />
+      </form>
         {this.state.cars.map(car =>
           <div key ={car.title} >
             <span>{car.title}</span>
@@ -67,7 +85,7 @@ class App extends Component {
           </div>
         )}
 
-        {this.state.list.map(item =>
+        {this.state.list.filter(isSearched(this.state.searchTerm)).map(item =>
           <div key={item.objectID}>
             <span>
               <a href={item.url}>{item.title}</a>
