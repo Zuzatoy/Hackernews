a8db6a3894da6414c770dfae871dff2823f38376
Lisa <zuzatoy@gmail.com>
2018-10-11 21:36:41 +1300

    API more functionality

--- src/App.js
+++ src/App.js
@@ -35,9 +35,14 @@ const cars = [
 
 
 const DEFAULT_QUERY = 'redux';
+const DEFAULT_HPP = '100';
+
 const PATH_BASE = 'https://hn.algolia.com/api/v1';
 const PATH_SEARCH = '/search';
 const PARAM_SEARCH = 'query=';
+const PARAM_PAGE = 'page=';
+const PARAM_HPP = 'hitsPerPage=';
+
 
 
 
@@ -59,12 +64,26 @@ class App extends Component {
   }
 
   setSearchTopStories(result) {
-    this.setState({ result });
+    const { hits,page } = result;
+
+    const oldHits = page !== 0
+    ? this.state.result.hits
+    : [];
+  const updatedHits = [
+    ...oldHits,
+    ...hits
+];
+
+
+    this.setState({ 
+      result: { hits: updatedHits, page } 
+    });
   }
 
 
-  fetchSearchTopStories(searchTerm) {
-    fetch(`${PATH_BASE}${PATH_SEARCH}?${PARAM_SEARCH}${searchTerm}`)
+  fetchSearchTopStories(searchTerm, page = 0) {
+    fetch(`${PATH_BASE}${PATH_SEARCH}?${PARAM_SEARCH}${searchTerm}&${PARAM_PAGE}${page}&${PARAM_HPP}${DEFAULT_HPP}`)
+
       .then(response => response.json())
       .then(result => this.setSearchTopStories(result))
       .catch(error => error);
@@ -96,8 +115,7 @@ class App extends Component {
 
   render() {
     const { searchTerm, result } = this.state;
-
-    if (!result) { return null; }
+    const page = (result && result.page) || 0;
     return (
       <div className="page">
         <div className="interactions">
@@ -109,10 +127,15 @@ class App extends Component {
           </Search>
           { result &&
            <Table
-          list={result.hits}
-          onDismiss={this.onDismiss}
-          />
-          }
+            list={result.hits}
+            onDismiss={this.onDismiss}
+          />  
+        }
+        <div className="interactions">
+            <Button onClick={() => this.fetchSearchTopStories(searchTerm, page + 1)}>
+                More
+            </Button>
+        </div>
         </div>
         </div>
         ); 
